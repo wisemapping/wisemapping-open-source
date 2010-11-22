862a53e147ee91a98dc80163d37d427196411bb6
Pablo <lunapablo@gmail.com>
2010-11-22 22:35:36 +0100

    Fixing test user data

--- wise-webapp/src/test/sql/hsql/test-data.sql
+++ wise-webapp/src/test/sql/hsql/test-data.sql
@@ -1,5 +1,5 @@
 INSERT INTO COLABORATOR(id,email,creation_date) values (1,'test@wisemapping.org',CURDATE());
 INSERT INTO USER (colaborator_id,username,firstname, lastname, password, activationCode,activation_date,allowSendEmail)
-values(1,'Wise Mapping Test User','Wise','test', 'test',1237,CURDATE(),1);
+values(1,'Wise Mapping Test User','Wise','test', 'ENC:a94a8fe5ccb19ba61c4c0873d391e987982fbbd3',1237,CURDATE(),1);
 COMMIT;
 SHUTDOWN;
