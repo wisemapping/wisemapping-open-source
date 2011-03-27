c74adcbf54b295e262a4119051f773c95f4df5d0
Paulo Gustavo Veiga <paulo@pveiga.com.ar>
2011-03-27 15:32:58 -0300

    Add node links tests. No working :).

---
+++ wise-webapp/src/test/data/freemind/links.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1301250606816" ID="ID_189563190" LINK="http://www.google.com" MODIFIED="1301250665708" TEXT="Node Links"/>
+</map>
