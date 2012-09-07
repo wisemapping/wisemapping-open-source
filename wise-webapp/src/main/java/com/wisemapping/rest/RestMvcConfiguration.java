8d9fb537415f7c2159c9b851b003bc0734640c4e
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2012-09-07 19:49:36 -0300

    Minor fix.

--- wise-webapp/src/main/java/com/wisemapping/rest/RestMvcConfiguration.java
+++ wise-webapp/src/main/java/com/wisemapping/rest/RestMvcConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import java.util.List;
 
+@EnableWebMvc
 @Configuration
 public class RestMvcConfiguration extends WebMvcConfigurerAdapter {
 
