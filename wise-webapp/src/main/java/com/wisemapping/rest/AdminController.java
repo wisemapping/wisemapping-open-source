c12b73c1ff7e74c473e87ebbac38747f6dbfbf4c
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2013-03-24 19:24:24 -0300

    Add catch ...

--- wise-webapp/src/main/java/com/wisemapping/rest/AdminController.java
+++ wise-webapp/src/main/java/com/wisemapping/rest/AdminController.java
@@ -142,27 +142,35 @@ public class AdminController extends BaseController {
                     continue;
                 }
 
-                // Iterate over the list of maps ...
-                final List<Collaboration> collaborations = mindmapService.findCollaborations(user);
-                for (Collaboration collaboration : collaborations) {
-                    final Mindmap mindmap = collaboration.getMindMap();
-                    if (MindmapFilter.MY_MAPS.accept(mindmap, user)) {
+                try {
+                    // Iterate over the list of maps ...
+                    final List<Collaboration> collaborations = mindmapService.findCollaborations(user);
+                    for (Collaboration collaboration : collaborations) {
+                        final Mindmap mindmap = collaboration.getMindMap();
+                        if (MindmapFilter.MY_MAPS.accept(mindmap, user)) {
 
-                        final Calendar yearAgo = Calendar.getInstance();
-                        yearAgo.add(Calendar.MONTH, -8);
-                        // The use has only two maps... When they have been modified ..
-                        System.out.println("Checking map id:" + mindmap.getId());
-                        if (mindmap.getLastModificationTime().before(yearAgo) && !mindmap.isPublic()) {
-                            System.out.println("Old map. Is simple ?:" + mindmap.getId());
+                            final Calendar yearAgo = Calendar.getInstance();
+                            yearAgo.add(Calendar.MONTH, -8);
+                            // The use has only two maps... When they have been modified ..
+                            System.out.println("Checking map id:" + mindmap.getId());
+                            if (mindmap.getLastModificationTime().before(yearAgo) && !mindmap.isPublic()) {
+                                System.out.println("Old map. Is simple ?:" + mindmap.getId());
 
-                            if (isWelcomeMap(mindmap) || isSimpleMap(mindmap)) {
-                                System.out.println("Purged map id:" + mindmap.getId() + ", userId:" + user.getId());
-                                if (apply) {
-                                    mindmapService.removeMindmap(mindmap, user);
+                                if (isWelcomeMap(mindmap) || isSimpleMap(mindmap)) {
+                                    System.out.println("Purged map id:" + mindmap.getId() + ", userId:" + user.getId());
+                                    if (apply) {
+                                        mindmapService.removeMindmap(mindmap, user);
+                                    }
                                 }
                             }
                         }
                     }
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } catch (WiseMappingException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                } catch (RuntimeException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                 }
             }
         }
