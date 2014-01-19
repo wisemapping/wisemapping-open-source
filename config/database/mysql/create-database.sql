09337865252ad712f06567911317099419339e02
Claudio Barril <claudiobarril@gmail.com>
2014-01-19 12:39:42 -0300

    fix mysql database and user creation

--- config/database/mysql/create-database.sql
+++ config/database/mysql/create-database.sql
@@ -1,6 +1,7 @@
+DROP DATABASE IF EXISTS wisemapping;
+
 CREATE DATABASE IF NOT EXISTS wisemapping
   CHARACTER SET = 'utf8'
   COLLATE = 'utf8_unicode_ci';
-CREATE USER 'wisemapping'@'localhost'
-  IDENTIFIED BY 'password';
 GRANT ALL ON wisemapping.* TO 'wisemapping'@'localhost';
+SET PASSWORD FOR 'wisemapping'@'localhost' = PASSWORD('password');
