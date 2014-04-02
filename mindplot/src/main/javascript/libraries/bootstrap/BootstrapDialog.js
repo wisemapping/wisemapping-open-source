31671d1c5e66d51294088dffdc2008129dd386ed
Mariela Michalek <mariela.michalek@gmail.com>
2014-04-02 15:19:54 -0300

    update bootstrapDialog

--- mindplot/src/main/javascript/libraries/bootstrap/BootstrapDialog.js
+++ mindplot/src/main/javascript/libraries/bootstrap/BootstrapDialog.js
@@ -1,31 +1,31 @@
 var BootstrapDialog = new Class({
 
     initialize: function () {
-        this.content = $('<div></div>');
+        this._native = $('<div></div>');
     },
 
 
     show: function (title) {
         $assert(title, "message can not be null");
 
-        var modalDialog = $('<div class="modal fade">' +
-            '<div class="modal-dialog">' +
-            '<div class="modal-content">' +
-            '<div class="modal-header">' +
-                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-                '<h3 class="modal-title">' + title + '</h3>' +
-            '</div>' +
-            '<div class="modal-body">' +
-            this.content.html() +
-            '</div>' +
-            '</div>' +
-            '</div>' +
+        var modalDialog = $(
+            '<div class="modal fade">' +
+                '<div class="modal-dialog">' +
+                    '<div class="modal-content">' +
+                        '<div class="modal-header">' +
+                            '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+                            '<h3 class="modal-title">' + title + '</h3>' +
+                        '</div>' +
+                        '<div class="modal-body">' +
+                            this._native.html() +
+                        '</div>' +
+                    '</div>' +
+                '</div>' +
             '</div>');
-
         modalDialog.modal();
     },
 
-    appendToContent:function (content){
-        this.content.append(content);
+    setContent:function (content){
+        this._native.append(content);
     }
 });
