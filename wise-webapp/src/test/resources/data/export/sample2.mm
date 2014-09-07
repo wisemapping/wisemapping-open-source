68b8428439b875e8b9689c7ec7da21c1caa9a9e1
Claudio Barril <claudiobarril@gmail.com>
2014-09-07 20:20:03 -0300

    fixing files to test export

--- wise-webapp/src/test/resources/data/export/sample2.mm
+++ wise-webapp/src/test/resources/data/export/sample2.mm
@@ -1,4 +1,4 @@
-<map version="0.9.0">
+<map version="1.0.1">
     <node BACKGROUND_COLOR="#f2981b" ID="ID_null" LINK="prospace.com" TEXT="Prospace">
         <edge COLOR="#cc5627"/>
         <richcontent TYPE="NOTE">
@@ -9,23 +9,23 @@
                 </body>
             </html>
         </richcontent>
-        <node ID="ID_null" POSITION="left" TEXT="Newspapers">
-            <node ID="ID_null" POSITION="left" TEXT="College Newspapers"/>
-        </node>
         <node ID="ID_null" POSITION="left" TEXT="Third Party ">
             <node ID="ID_null" LINK="thumbshots.org" POSITION="left" STYLE="rectagle" TEXT="Thumbshot"/>
+        </node>
+        <node ID="ID_null" POSITION="left" TEXT="Newspapers">
+            <node ID="ID_null" POSITION="left" TEXT="College Newspapers"/>
         </node>
         <node ID="ID_null" POSITION="right" TEXT="Partnerships">
             <node ID="ID_null" POSITION="left" TEXT="Websites">
                 <node ID="ID_null" POSITION="left" TEXT="Business Networks">
-                    <node ID="ID_null" POSITION="left" TEXT="Xing"/>
                     <node ID="ID_null" POSITION="left" TEXT="konnects.com"/>
+                    <node ID="ID_null" POSITION="left" TEXT="Xing"/>
                 </node>
             </node>
         </node>
         <node ID="ID_null" POSITION="left" TEXT="Career Counseling">
-            <node ID="ID_null" POSITION="left" TEXT="Fraternity"/>
             <node ID="ID_null" POSITION="left" TEXT="Professional Associations"/>
+            <node ID="ID_null" POSITION="left" TEXT="Fraternity"/>
         </node>
         <node ID="ID_null" POSITION="left" TEXT="White-Label">
             <node ID="ID_null" POSITION="left" TEXT="http://www.harrisconnect.com/"/>
