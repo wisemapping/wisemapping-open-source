d8e255cac8a262b4c20042b9aff467fd5fdd590c
Claudio Barril <claudiobarril@gmail.com>
2014-09-07 20:16:09 -0300

    considering possibility of null positions

--- wise-webapp/src/main/java/com/wisemapping/exporter/VerticalPositionComparator.java
+++ wise-webapp/src/main/java/com/wisemapping/exporter/VerticalPositionComparator.java
@@ -10,9 +10,17 @@ public class VerticalPositionComparator implements Comparator<TopicType> {
     public int compare(TopicType o1, TopicType o2) {
         final String myPosition = o1.getPosition();
         final String otherPosition = o2.getPosition();
-        int myPositionY = Integer.parseInt(myPosition.split(",")[1]);
-        int otherPositionY = Integer.parseInt(otherPosition.split(",")[1]);
-        return myPositionY - otherPositionY;
+        int result;
+        if (otherPosition == null) {
+            result = -1;
+        } else if (myPosition == null) {
+            result = 1;
+        } else {
+            int myPositionY = Integer.parseInt(myPosition.split(",")[1]);
+            int otherPositionY = Integer.parseInt(otherPosition.split(",")[1]);
+            result = myPositionY - otherPositionY;
+        }
+        return result;
     }
 
 }
