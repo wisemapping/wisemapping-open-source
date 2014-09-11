71b9357a9fdcc8c324d05658973dfda170d70a98
Ezequiel Bergamaschi <ezequielbergamaschi@gmail.com>
2014-09-11 14:38:22 -0300

    fix esc key not working if not focus in modal

--- mindplot/src/main/javascript/libraries/bootstrap/BootstrapDialog.js
+++ mindplot/src/main/javascript/libraries/bootstrap/BootstrapDialog.js
@@ -13,7 +13,7 @@ var BootstrapDialog = new Class({
     initialize: function (title, options) {
         this.setOptions(options);
         this.options.onRemoveClickData.dialog = this;
-        this._native = $('<div class="modal fade"></div>').append('<div class="modal-dialog"></div>');
+        this._native = $('<div class="modal fade" tabindex="-1"></div>').append('<div class="modal-dialog"></div>');
         var content = $('<div class="modal-content"></div>');
         content.on('keydown', function(event) {
             event.stopPropagation();
