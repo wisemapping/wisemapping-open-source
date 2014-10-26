00ab6fd61afdbe564f3677e7cd7c0df7bc0f21a7
Ezequiel Bergamaschi <ezequielbergamaschi@gmail.com>
2014-10-26 18:43:16 -0300

    adding tests to suite

--- mindplot/src/test/javascript/DesignerTestSuite.js
+++ mindplot/src/test/javascript/DesignerTestSuite.js
@@ -1,69 +1,68 @@
-var setup = function() {
-    /*:DOC += <div id="mindplot"></div> */
-    var mapId = '1';
-    var mapXml = '<map name="1" version="pela"><topic central="true" text="test" id="1"><topic position="103,-52" order="" id="2"/><topic position="-134,-75" order="" id="3"/><topic position="-126,5" order="" id="4"/><topic position="-115,53" order="" id="5"/><topic position="-136,-35" order="" id="6"/></topic></map>';
-    var editorProperties = {"zoom":0.7, size: {width: "1366px", height:"768px"}};
-    var buildMindmapDesigner = function() {
+var designer = null;
+/*:DOC += <div id="mindplot"></div> */
+var mapId = '1';
+var mapXml = '<map name="1" version="pela"><topic central="true" text="test" id="1"><topic position="103,-52" order="" id="2"/><topic position="-134,-75" order="" id="3"/><topic position="-126,5" order="" id="4"/><topic position="-115,53" order="" id="5"/><topic position="-136,-35" order="" id="6"/></topic></map>';
+var editorProperties = {"zoom":0.7, size: {width: "1366px", height:"768px"}};
+var buildMindmapDesigner = function() {
 
-        // Initialize message logger ...
-        var container = $('<div id="mindplot"></div>');
-        $("body").append(container);
+    // Initialize message logger ...
+    var container = $('<div id="mindplot"></div>');
+    $("body").append(container);
 
-        // Initialize Editor ...
-        var window = $(window);
-        var screenWidth = window.width();
-        var screenHeight = window.height();
+    // Initialize Editor ...
+    var window = $(window);
+    var screenWidth = window.width();
+    var screenHeight = window.height();
 
-        // Positionate node ...
-        // header - footer
-        screenHeight = screenHeight - 90 - 61;
+    // Positionate node ...
+    // header - footer
+    screenHeight = screenHeight - 90 - 61;
 
-        // body margin ...
-        editorProperties.width = screenWidth;
-        editorProperties.height = screenHeight;
+    // body margin ...
+    editorProperties.width = screenWidth;
+    editorProperties.height = screenHeight;
 
-        var designer = new mindplot.Designer(editorProperties, container);
-        /*var persistence = mindplot.PersistenceManager.getInstance();
-        designer.loadMap(mapId, mapXml);
-*/
+    designer = new mindplot.Designer(editorProperties, container);
+    // Load map from XML file persisted on disk...
+    var persistence = new mindplot.LocalStorageManager("src/test/resources/welcome.xml");
+    var mindmap = persistence.load(mapId);
+    designer.loadMap(mindmap);
 
-        /*// Save map on load ....
-         if (editorProperties.saveOnLoad)
-         {
-         var saveOnLoad = function() {
-         designer.save(function() {
-         }, false);
-         }.delay(1000)
-         }*/
+    /*// Save map on load ....
+     if (editorProperties.saveOnLoad)
+     {
+     var saveOnLoad = function() {
+     designer.save(function() {
+     }, false);
+     }.delay(1000)
+     }*/
 
-    }
-    buildMindmapDesigner();
 }
+buildMindmapDesigner();
 
 
 describe("Designer test suite", function() {
 
-    beforeEach(function() {
-        //setup();
-    });
     it("testWorkspaceBuild", function(){
-        var id = $(document).attr('id');
-        expect(id).not.toBeNull();
-        //expect(id).not.toBeUndefined();
+        var mindplot = $(document).find('#mindplot');
+        expect(mindplot).not.toBeNull();
+        expect(mindplot).not.toBeUndefined();
     });
-    /*testCentralTopicPresent:function(){
-        var centralTopic = designer.getCentralTopic();
-        assertNotNull(centralTopic);
+    it("testCentralTopicPresent", function(){
+        var centralTopic = designer.getMindmap().getCentralTopic();
+        expect($defined(centralTopic)).toBe(true);
         var position = centralTopic.getPosition();
-        assertEquals(0,position.x);
-        assertEquals(0,position.y);
-    },
-    testMouseCreateMainTopic:function(){
-        var centralTopic = designer.getCentralTopic();
-        assertNotNull(centralTopic);
+        expect(position.x).toEqual(0);
+        expect(position.y).toEqual(0);
+    });
+    /*it("testCentralTopicPresent", function(){
+        var centralTopic = designer.getMindmap().getCentralTopic();
+        expect($defined(centralTopic)).toBe(true);
         var target = designer.getWorkSpace().getScreenManager().getContainer();
         var size = designer.getModel().getTopics().length;
         fireNativeEvent('dblclick',target,new core.Point(50,50));
         assertEquals(size+1, designer.getModel().getTopics().length);
-    }*/
+    });
+    */
+
 });
