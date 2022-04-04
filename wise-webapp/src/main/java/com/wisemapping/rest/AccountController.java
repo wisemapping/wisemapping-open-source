86a0a876cadf1162a442ab2685cf1b72b29c7272
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2022-04-03 22:02:20 -0300

    Fix error trying to delete account with labels created.

--- wise-webapp/src/main/java/com/wisemapping/rest/AccountController.java
+++ wise-webapp/src/main/java/com/wisemapping/rest/AccountController.java
@@ -1,31 +1,33 @@
 /*
-*    Copyright [2022] [wisemapping]
-*
-*   Licensed under WiseMapping Public License, Version 1.0 (the "License").
-*   It is basically the Apache License, Version 2.0 (the "License") plus the
-*   "powered by wisemapping" text requirement on every single page;
-*   you may not use this file except in compliance with the License.
-*   You may obtain a copy of the license at
-*
-*       http://www.wisemapping.org/license
-*
-*   Unless required by applicable law or agreed to in writing, software
-*   distributed under the License is distributed on an "AS IS" BASIS,
-*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*   See the License for the specific language governing permissions and
-*   limitations under the License.
-*/
+ *    Copyright [2022] [wisemapping]
+ *
+ *   Licensed under WiseMapping Public License, Version 1.0 (the "License").
+ *   It is basically the Apache License, Version 2.0 (the "License") plus the
+ *   "powered by wisemapping" text requirement on every single page;
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the license at
+ *
+ *       http://www.wisemapping.org/license
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 
 package com.wisemapping.rest;
 
 import com.wisemapping.exceptions.WiseMappingException;
 import com.wisemapping.mail.NotificationService;
 import com.wisemapping.model.Collaboration;
+import com.wisemapping.model.Label;
 import com.wisemapping.model.Mindmap;
 import com.wisemapping.model.User;
 import com.wisemapping.rest.model.RestLogItem;
 import com.wisemapping.rest.model.RestUser;
 import com.wisemapping.security.Utils;
+import com.wisemapping.service.LabelService;
 import com.wisemapping.service.MindmapService;
 import com.wisemapping.service.UserService;
 import org.apache.log4j.Logger;
@@ -51,6 +53,10 @@ public class AccountController extends BaseController {
     @Qualifier("mindmapService")
     @Autowired
     private MindmapService mindmapService;
+
+    @Qualifier("labelService")
+    @Autowired
+    private LabelService labelService;
 
     @Autowired
     private NotificationService notificationService;
@@ -113,15 +119,26 @@ public class AccountController extends BaseController {
 
     @RequestMapping(method = RequestMethod.DELETE, value = "account")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void deleteUser() throws WiseMappingException
-
-    {
+    public void deleteUser() throws WiseMappingException {
+        // Delete collaborations ...
         final User user = Utils.getUser(true);
         final List<Collaboration> collaborations = mindmapService.findCollaborations(user);
         for (Collaboration collaboration : collaborations) {
             final Mindmap mindmap = collaboration.getMindMap();
-            mindmapService.removeMindmap(mindmap,user);
+            mindmapService.removeMindmap(mindmap, user);
         }
+
+        // Delete labels ....
+        List<Label> labels = labelService.getAll(user);
+        labels.forEach(l -> {
+            try {
+                labelService.removeLabel(l, user);
+            } catch (WiseMappingException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+
+        // Finally, delete user ...
         userService.removeUser(user);
     }
 }
