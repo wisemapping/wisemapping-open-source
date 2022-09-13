242906d6d4f4628d5f3a743e8068999ad42135d3
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2022-09-12 21:58:49 -0700

    Fix layout specification

--- distribution/build-image.sh
+++ distribution/build-image.sh
@@ -4,4 +4,4 @@ set -o
 set -u
 
 mvn -f ../pom.xml clean package
-docker build -t wisemapping/wisemapping:latest -f ./Dockerfile ../wise-webapp/target/
+docker build --platform linux/amd64 -t wisemapping/wisemapping:latest -f ./Dockerfile ../wise-webapp/target/
