fa06852806904044bee5b8a16164bd2f868f336b
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2023-11-27 21:47:22 -0800

    Add static resources.

--- wise-webapp/src/main/java/com/wisemapping/config/mvc/MvcAppConfig.java
+++ wise-webapp/src/main/java/com/wisemapping/config/mvc/MvcAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -18,12 +19,12 @@ import org.springframework.web.servlet.view.JstlView;
 @ImportResource(value = {"classpath:spring/wisemapping-servlet.xml"})
 @ComponentScan("com.wisemapping.webmvc")
 public class MvcAppConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry
-//                .addResourceHandler("/**")
-//                .addResourceLocations("classpath:/public/");
-//    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/**")
+                .addResourceLocations("classpath:/public/");
+    }
 
     @Bean
     public ViewResolver viewResolver() {
