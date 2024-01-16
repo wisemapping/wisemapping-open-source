c99d7837a321f2cc499db6cbd73a1e47f854ee1a
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2024-01-15 18:10:01 -0800

    Remove icon as mandatory

--- config/database/postgres/create-schemas.sql
+++ config/database/postgres/create-schemas.sql
@@ -27,7 +27,6 @@ CREATE TABLE "LABEL" (
   creator_id      INTEGER            NOT NULL,
   parent_label_id INTEGER,
   color           VARCHAR(7)         NOT NULL,
-  iconName        VARCHAR(50)       NOT NULL,
   FOREIGN KEY (creator_id) REFERENCES "USER" (colaborator_id)
 );
 
