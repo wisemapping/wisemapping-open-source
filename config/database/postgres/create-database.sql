aadb88451bfa3953a9a083701ac6aea39eaae0e9
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2024-02-17 00:14:09 -0800

    Mysql

--- config/database/postgres/create-database.sql
+++ config/database/postgres/create-database.sql
@@ -1,4 +1,7 @@
+
 CREATE DATABASE wisemapping;
 CREATE USER wisemapping WITH PASSWORD 'password';
-
 GRANT ALL PRIVILEGES ON DATABASE wisemapping TO wisemapping;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO wisemapping;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO wisemapping;
