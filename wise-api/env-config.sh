454ecddd76bf2880398913a827fdc6fe13b4aa6a
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2025-09-01 18:00:34 -0300

    Update env variable name.

--- wise-api/env-config.sh
+++ wise-api/env-config.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Check if SPRING_APP_CONFIG_VALUE is empty
-if [ -z "$SPRING_APP_CONFIG_VALUE" ]; then
-    echo "ERROR: SPRING_APP_CONFIG_VALUE environment variable is empty or not set. Using default local configuration." >&2
+# Check if SPRING_CONFIG_ADDITIONAL_FILE_CONTENT is empty
+if [ -z "SPRING_CONFIG_ADDITIONAL_FILE_CONTENT" ]; then
+    echo "ERROR: SPRING_CONFIG_ADDITIONAL_FILE_CONTENT environment variable is empty or not set. Using default local configuration." >&2
 fi
 
 # Write it to a file
-echo "$SPRING_APP_CONFIG_VALUE" > /app/app.yml
+echo "$SPRING_CONFIG_ADDITIONAL_FILE_CONTENT" > /app/app.yml
