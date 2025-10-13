b48bec48607debb4d3ea9f9e52407a8d936a3ff4
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2025-10-13 16:32:58 -0300

    Improve email sent logic.

--- wise-api/src/main/java/com/wisemapping/service/NotificationService.java
+++ wise-api/src/main/java/com/wisemapping/service/NotificationService.java
@@ -150,9 +150,13 @@ final public class NotificationService {
 
 
     public void activateAccount(@NotNull Account user) {
-        final Map<String, Object> model = new HashMap<>();
-        model.put("user", user);
-        mailerService.sendEmail(mailerService.getServerSenderEmail(), user.getEmail(), "[WiseMapping] Active account", model, "activationAccountMail.vm");
+        try {
+            final Map<String, Object> model = new HashMap<>();
+            model.put("user", user);
+            mailerService.sendEmail(mailerService.getServerSenderEmail(), user.getEmail(), "[WiseMapping] Active account", model, "activationAccountMail.vm");
+        } catch (Exception e) {
+            logger.error("Error sending account activation confirmation email to: " + user.getEmail(), e);
+        }
     }
 
     public void sendRegistrationEmail(@NotNull Account user) {
