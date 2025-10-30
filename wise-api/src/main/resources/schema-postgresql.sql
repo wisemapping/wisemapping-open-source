ab54ea8c4508d7f4cf004888ee34be51f7a9b01e
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2025-10-30 13:07:55 -0300

    Add migration scripts for indexes on MINDMAP_SPAM_INFO to optimize query performance
    - Created new migration SQL files for HSQLDB, MySQL, and PostgreSQL to add indexes on the MINDMAP_SPAM_INFO table, enhancing performance for spam detection and filtering queries.
    - Included comments in the migration files to explain the purpose and benefits of the indexes.
    - Removed index creation from schema files to avoid duplication and ensure indexes are only created during migration.

--- wise-api/src/main/resources/schema-postgresql.sql
+++ wise-api/src/main/resources/schema-postgresql.sql
@@ -59,9 +59,6 @@ CREATE TABLE IF NOT EXISTS MINDMAP_SPAM_INFO (
     ON UPDATE NO ACTION
 );
 
-CREATE INDEX IF NOT EXISTS idx_spam_detected ON MINDMAP_SPAM_INFO(spam_detected);
-CREATE INDEX IF NOT EXISTS idx_spam_detection_version ON MINDMAP_SPAM_INFO(spam_detection_version);
-
 create TABLE IF NOT EXISTS R_LABEL_MINDMAP (
   mindmap_id       INTEGER            NOT NULL,
   label_id         INTEGER            NOT NULL,
