0abb5e9bcb1ca92ff80922c47a4048e6f06db395
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2025-10-30 10:14:17 -0300

    Remove unsed imports.

--- wise-api/src/test/java/com/wisemapping/scheduler/SchedulerUnitTest.java
+++ wise-api/src/test/java/com/wisemapping/scheduler/SchedulerUnitTest.java
@@ -20,13 +20,12 @@ package com.wisemapping.scheduler;
 
 import com.wisemapping.service.SpamDetectionBatchService;
 import com.wisemapping.service.SpamUserSuspensionService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.util.Re flectionTestUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
